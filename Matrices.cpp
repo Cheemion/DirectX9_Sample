38945199b6394b219047c798cb9d66139977bda5
kimwang2 <Kim.Wang@amd.com>
2023-04-13 14:49:38 +0800

    vertex shader enabled

--- Matrices.cpp
+++ Matrices.cpp
@@ -57,6 +57,7 @@ struct Vertex
         _x = x;  _y = y;  _z = z;
     }
     float _x, _y, _z;
+    D3DCOLOR _color;
     static const DWORD FVF;
 };
 const DWORD Vertex::FVF = D3DFVF_XYZ;
@@ -71,8 +72,6 @@ struct CUSTOMVERTEX
 // Our custom FVF, which describes our custom vertex structure
 #define D3DFVF_CUSTOMVERTEX (D3DFVF_XYZ|D3DFVF_DIFFUSE)
 
-
-VOID SetupMatrices();
 
 //-----------------------------------------------------------------------------
 // Name: InitD3D()
@@ -110,7 +109,6 @@ HRESULT InitD3D( HWND hWnd )
         return E_FAIL;
     }
 
-
     return S_OK;
 }
 
@@ -143,28 +141,10 @@ HRESULT Init()
     /* Create Vertex Shader */
 
     /*create Vertex Buffer And Index Buffer*/
-    hr = g_pd3dDevice->CreateVertexBuffer( 8 * sizeof(Vertex),
-									D3DUSAGE_WRITEONLY,
-									Vertex::FVF,
-									D3DPOOL_MANAGED,
-									&g_vertexBuffer,
-									0);
+    hr = g_pd3dDevice->CreateVertexBuffer( 8 * sizeof(Vertex), D3DUSAGE_WRITEONLY, Vertex::FVF, D3DPOOL_MANAGED, &g_vertexBuffer, 0);
 
-    hr = g_pd3dDevice->CreateIndexBuffer(
-										36 * sizeof(WORD),
-										D3DUSAGE_WRITEONLY,
-										D3DFMT_INDEX16,
-										D3DPOOL_MANAGED,
-										&g_indexBuffer,
-										0);
-
+    hr = g_pd3dDevice->CreateIndexBuffer( 36 * sizeof(WORD), D3DUSAGE_WRITEONLY, D3DFMT_INDEX16, D3DPOOL_MANAGED, &g_indexBuffer, 0);
     /*create Vertex Buffer And Index Buffer*/
-
-
-    
-
-
-
 
     return S_OK;
 }
@@ -223,13 +203,23 @@ VOID Render()
 
     g_indexBuffer->Unlock();
 
+    /* apply mvp transformational matrix */
+	D3DXMATRIX world;
+	// incremement y-rotation angle each frame
+	static float y = 0.0f;
+	D3DXMatrixRotationY(&world, y);
+	y += 0.0001;
+
+	// reset angle to zero when angle reaches 2*PI
+	if (y >= 6.28f)
+		y = 0.0f;
+
     D3DXVECTOR3 position(0.0f, 0.0f, -5.0f);
     D3DXVECTOR3 target(0.0f, 0.0f, 0.0f);
     D3DXVECTOR3 up(0.0f, 1.0f, 0.0f);
     D3DXMATRIX V;
     D3DXMatrixLookAtLH(&V, &position, &target, &up);
 
-    g_pd3dDevice->SetTransform(D3DTS_VIEW, &V);
 
     D3DXMATRIX proj;
     D3DXMatrixPerspectiveFovLH(
@@ -238,22 +228,15 @@ VOID Render()
         (float)WIDTH / (float)HEIGHT,
         1.0f,
         1000.0f);
-    g_pd3dDevice->SetTransform(D3DTS_PROJECTION, &proj);
 
     g_pd3dDevice->SetRenderState(D3DRS_FILLMODE, D3DFILL_SOLID);
 
+    hr = g_pd3dDevice->SetVertexShader(g_vertexShader);
 
-    /* Fill the index buffer.  */
 
-    //hr =  g_pd3dDevice->SetVertexDeclaration(g_pDecl);
-
-    //hr = g_pd3dDevice->SetVertexShader(g_vertexShader);
-
-    // Turn off culling, so we see the front and back of the triangle
-    //g_pd3dDevice->SetRenderState( D3DRS_CULLMODE, D3DCULL_NONE );
-
-    // Turn off D3D lighting, since we are providing our own vertex colors
-    //g_pd3dDevice->SetRenderState( D3DRS_LIGHTING, FALSE );
+    D3DXHANDLE h = g_constTable->GetConstantByName(0, "mWorldViewProj");
+    D3DXMATRIXA16 temp = world * V * proj;
+    g_constTable->SetMatrix(g_pd3dDevice, h, (const D3DXMATRIX*)&temp);
 
     // Clear the backbuffer to a black color
     g_pd3dDevice->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0xffffffff, 1.0f, 0);
@@ -261,46 +244,13 @@ VOID Render()
     // Begin the scene
     if( SUCCEEDED( g_pd3dDevice->BeginScene() ) )
     {
-        // Setup the world, view, and projection Matrices
-        //SetupMatrices();
-
-        // Render the vertex buffer contents
-
-            //
-        // spin the cube:
-        //
-        D3DXMATRIX Rx, Ry;
-
-        // rotate 45 degrees on x-axis
-        D3DXMatrixRotationX(&Rx, 3.14f / 4.0f);
-
-        // incremement y-rotation angle each frame
-        static float y = 0.0f;
-        D3DXMatrixRotationY(&Ry, y);
-        y += 0.0001;
-
-        // reset angle to zero when angle reaches 2*PI
-        if (y >= 6.28f)
-            y = 0.0f;
-
-        // combine x- and y-axis rotation transformations.
-        D3DXMATRIX p = Rx * Ry;
-
-        g_pd3dDevice->SetTransform(D3DTS_WORLD, &p);
-
-        g_pd3dDevice->SetStreamSource( 0, g_vertexBuffer, 0, sizeof( Vertex ) );
-        g_pd3dDevice->SetIndices( g_indexBuffer );
-        g_pd3dDevice->SetFVF(Vertex::FVF);
-
-        //g_pd3dDevice->SetFVF( D3DFVF_CUSTOMVERTEX );
-		//g_pd3dDevice->DrawPrimitive( D3DPT_TRIANGLESTRIP, 0, 1 );
-
+        hr = g_pd3dDevice->SetStreamSource( 0, g_vertexBuffer, 0, sizeof( Vertex ) );
+        hr = g_pd3dDevice->SetIndices( g_indexBuffer );
+		hr = g_pd3dDevice->SetVertexDeclaration(g_pDecl);
         g_pd3dDevice->DrawIndexedPrimitive(D3DPT_TRIANGLELIST, 0, 0, 8, 0, 12);
-
         // End the scene
         g_pd3dDevice->EndScene();
     }
-
     // Present the backbuffer contents to the display
     g_pd3dDevice->Present( NULL, NULL, NULL, NULL );
 }
@@ -328,49 +278,6 @@ VOID Cleanup()
 // Name: SetupMatrices()
 // Desc: Sets up the world, view, and projection transform Matrices.
 //-----------------------------------------------------------------------------
-VOID SetupMatrices()
-{
-    // For our world matrix, we will just rotate the object about the y-axis.
-    D3DXMATRIXA16 matWorld;
-
-    // Set up the rotation matrix to generate 1 full rotation (2*PI radians) 
-    // every 1000 ms. To avoid the loss of precision inherent in very high 
-    // floating point numbers, the system time is modulated by the rotation 
-    // period before conversion to a radian angle.
-    UINT iTime = timeGetTime() % 1000;
-    FLOAT fAngle = iTime * ( 2.0f * D3DX_PI ) / 1000.0f;
-    D3DXMatrixRotationY( &matWorld, fAngle );
-    g_pd3dDevice->SetTransform( D3DTS_WORLD, &matWorld );
-
-    // Set up our view matrix. A view matrix can be defined given an eye point,
-    // a point to lookat, and a direction for which way is up. Here, we set the
-    // eye five units back along the z-axis and up three units, look at the
-    // origin, and define "up" to be in the y-direction.
-    D3DXVECTOR3 vEyePt( 0.0f, 3.0f,-5.0f );
-    D3DXVECTOR3 vLookatPt( 0.0f, 0.0f, 0.0f );
-    D3DXVECTOR3 vUpVec( 0.0f, 1.0f, 0.0f );
-    D3DXMATRIXA16 matView;
-    D3DXMatrixLookAtLH( &matView, &vEyePt, &vLookatPt, &vUpVec );
-    g_pd3dDevice->SetTransform( D3DTS_VIEW, &matView );
-
-    // For the projection matrix, we set up a perspective transform (which
-    // transforms geometry from 3D view space to 2D viewport space, with
-    // a perspective divide making objects smaller in the distance). To build
-    // a perpsective transform, we need the field of view (1/4 pi is common),
-    // the aspect ratio, and the near and far clipping planes (which define at
-    // what distances geometry should be no longer be rendered).
-    D3DXMATRIXA16 matProj;
-    D3DXMatrixPerspectiveFovLH( &matProj, D3DX_PI / 4, 1.0f, 1.0f, 100.0f );
-    g_pd3dDevice->SetTransform( D3DTS_PROJECTION, &matProj );
-
-
-    D3DXHANDLE h = g_constTable->GetConstantByName(0, "mWorldViewProj");
-    D3DXMATRIXA16 temp = matWorld* matView* matProj;
-    g_constTable->SetMatrix(g_pd3dDevice, h, (const D3DXMATRIX*)&temp);
-
-}
-
-
 
 
 
